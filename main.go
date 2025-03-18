c8bccd94284faa0e2807ffc843171f02fc740bc4
Marcos Couto <marcosmartinellicouto@gmail.com>
2025-03-18 11:12:37 -0300

    chore: list steps to do

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Ler os arquivos da pasta migrations e listar os nomes
+		// Verficar se todos os arquivos tem o pattern correto (V0_nome.sql)
+		// Verificar se todos os SQLs estão corretos
+		// Verificar se todos os arquivos estão na ordem correta e se não existe repetição
+	
+	// Executar as migrations
+	// Salvar o resultado da execução das migrations
 }
