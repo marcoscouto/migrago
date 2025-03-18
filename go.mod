53521cc83668799764bda4267d0f1392d491d68e
Marcos Couto <marcosmartinellicouto@gmail.com>
2025-03-18 16:47:34 -0300

    feat: read `files` and execute `validations`

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marcoscouto/migo
 
 go 1.24.1
+
+require github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
 
 func main() {
 	// Ler os arquivos da pasta migrations e listar os nomes
-		// Verficar se todos os arquivos tem o pattern correto (V0_nome.sql)
-		// Verificar se todos os SQLs estão corretos
-		// Verificar se todos os arquivos estão na ordem correta e se não existe repetição
+	files, err := os.ReadDir("./migrations")
+	if err != nil {
+		panic(err)
+	}
+
+	versions := make(map[string]bool)
+
+	for _, f := range files {
+		// Verificar se todos os arquivos tem o pattern correto (V0_nome.sql)
+		pattern, err := regexp.Compile(`^(\d+)_(\w+)\.sql$`)
+		if err != nil {
+			panic(err)
+		}
+
+		match := pattern.Match([]byte(f.Name()))
+		if !match {
+			err := fmt.Errorf("the pattern of %s don't match", f.Name())
+			panic(err)
+		}
+
+		// Verificar se todos os arquivos e se não existe repetição
+		matches := pattern.FindStringSubmatch(f.Name())
+		version := matches[1]
+		if _, ok := versions[version]; ok {
+			err := fmt.Errorf("duplicated version %s", version)
+			panic(err)
+		}
+		versions[version] = true
+
+		// Captura o conteúdo do arquivo
+		// content, err := os.ReadFile(fmt.Sprint("./migrations/", f.Name()))
+		// if err != nil {
+		// 	panic(err)
+		// }
+	}
 	
 	// Executar as migrations
 	// Salvar o resultado da execução das migrations
+
+	fmt.Println("executed successfully")
 }
